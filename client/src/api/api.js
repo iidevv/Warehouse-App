a1d78b07593543d5204891370ef790fe44fe71f0
Ilya Ilyich <mariomorales@Ilya-I.local>
2023-03-27 14:17:56 -0700

    api url fix

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const useHttps = process.env.REACT_APP_USE_HTTPS === "true";
 
 const instance = axios.create({
-  baseURL: useHttps ? "https://localhost:3001" : "http://localhost:3001",
+  baseURL: useHttps ? "https://warehouse.discountmotogear.com/" : "http://localhost:3001",
 });
 
 
