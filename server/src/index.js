71d59b5c63b7fa9cbc10c6bba9e2ecb9a192650a
Ilya Ilyich <mariomorales@Ilya-I.local>
2023-03-29 10:00:29 -0700

    http

--- server/src/index.js
+++ server/src/index.js
@@ -22,7 +22,7 @@ const dbPassword = process.env.DB_PASSWORD;
 const useHttps = process.env.USE_HTTPS === "true";
 const app = express();
 
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, "../client/build")));
 
 app.use(express.json());
 app.use(cors());
@@ -38,18 +38,6 @@ mongoose.connect(
   `mongodb+srv://${dbUsername}:${dbPassword}@dmg.eqxtdze.mongodb.net/${dbname}?retryWrites=true&w=majority`
 );
 
-if (useHttps) {
-  const privateKey = fs.readFileSync(process.env.SSL_KEY_PATH, "utf8");
-  const certificate = fs.readFileSync(process.env.SSL_CERT_PATH, "utf8");
-  const credentials = { key: privateKey, cert: certificate };
-
-  const httpsServer = https.createServer(credentials, app);
-
-  httpsServer.listen(port, () =>
-    console.log(`Server started on https://localhost:${port}`)
-  );
-} else {
-  app.listen(port, () =>
-    console.log(`Server started on http://localhost:${port}`)
-  );
-}
+app.listen(port, () =>
+  console.log(`Server started on http://localhost:${port}`)
+);
