9e1fecadfc0fb5b38ef06ff05d82a74db15dd01c
Ilya Ilyich <mariomorales@Ilya-I.local>
2023-04-04 08:40:12 -0700

    +6 hours createNewDate

--- server/src/common/index.js
+++ server/src/common/index.js
@@ -2,7 +2,11 @@ export const createNewDate = () => {
   const options = {
     timeZone: "America/Los_Angeles",
   };
+
   const date = new Date();
-  const dateString = date.toLocaleString("en-US", options);
+  const sixHoursInMilliseconds = 6 * 60 * 60 * 1000; // 6 hours * 60 minutes * 60 seconds * 1000 milliseconds
+  const adjustedDate = new Date(date.getTime() + sixHoursInMilliseconds);
+  const dateString = adjustedDate.toLocaleString("en-US", options);
+
   return dateString;
 };
