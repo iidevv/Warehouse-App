e9a3855ef06883a3210e6a86c2fd0a73167e6c42
Ilya Ilyich <mariomorales@Ilya-I.local>
2023-07-12 14:10:43 -0700

    tg id check

--- server/src/routes/tg-notifications.js
+++ server/src/routes/tg-notifications.js
@@ -6,6 +6,8 @@ const tgBot = new TelegramBot(token, { polling: true });
 
 tgBot.on("message", (msg) => {
   const chatId = msg.chat.id;
+  console.log(chatId);
+  console.log(msg.text);
   if (msg.text === "My id") {
     tgBot.sendMessage(`Your ID: ${chatId}`);
   }
