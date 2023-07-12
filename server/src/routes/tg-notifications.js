d69aa8e91a47e4b41aaf7928c64b5688724a7c15
Ilya Ilyich <mariomorales@Ilya-I.local>
2023-07-12 15:45:38 -0700

    tg id check

--- server/src/routes/tg-notifications.js
+++ server/src/routes/tg-notifications.js
@@ -6,20 +6,21 @@ const chatIds = process.env.TG_CHAT_IDS.split(",").map(Number);
 
 let tgBot;
 
-if (useHttps) {
-  tgBot = new TelegramBot(token, { polling: true });
+// if (useHttps) {
+//   tgBot = new TelegramBot(token, { polling: true });
 
-  tgBot.on("message", (msg) => {
-    const chatId = msg.chat.id;
-    if (msg.text === "My id") {
-      tgBot.sendMessage(chatId, `${chatId}`);
-    }
-  });
-}
+//   tgBot.on("message", (msg) => {
+//     const chatId = msg.chat.id;
+//     if (msg.text === "My id") {
+//       tgBot.sendMessage(chatId, `${chatId}`);
+//     }
+//   });
+// }
 
 export const sendNotification = (message = "", bot = tgBot, ids = chatIds) => {
   if (!bot) return;
   ids.forEach((id) => {
-    tgBot.sendMessage(id, message);
+    console.log(message);
+    // tgBot.sendMessage(id, message);
   });
 };
