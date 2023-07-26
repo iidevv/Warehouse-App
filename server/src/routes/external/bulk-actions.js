4bace055129ab47cf77732ec30b9a41a5a18f0a4
Ilya Ilyich <mariomorales@Ilya-I.local>
2023-07-26 12:42:20 -0700

    bulk video upload page 30

--- server/src/routes/external/bulk-actions.js
+++ server/src/routes/external/bulk-actions.js
@@ -37,7 +37,7 @@ async function asyncForEach(array, callback, concurrency = 5) {
 
 router.get("/bulk-action/", async (req, res) => {
   const pageSize = 5;
-  let currentPage = 1;
+  let currentPage = 29;
   let totalPages = 1;
   let totalVideos = 0;
   while (currentPage <= totalPages) {
