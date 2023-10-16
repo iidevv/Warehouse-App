08b4c90f4dbb81c810bc1770cc5fe49932b83ae5
Ilya Ilyich <mariomorales@Ilya-I.local>
2023-10-16 13:47:26 -0700

    delay for batches

--- server/src/middleLayer/turnMiddleLayer.js
+++ server/src/middleLayer/turnMiddleLayer.js
@@ -96,7 +96,7 @@ const createItems = async (items) => {
   for (let i = 0; i < items.length; i += batchSize) {
     const batch = items.slice(i, i + batchSize);
     await createItemsInBatch(batch);
-    // await delay(200);
+    await delay(100);
   }
 };
 
